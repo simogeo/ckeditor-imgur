66c5bf8b0598b66956edac4ad814711be10b33cb
Simon Georget <simon@linea21.com>
2017-05-09 08:32:15 +0200

    Create fr.js

---
+++ vendor/assets/javascripts/ckeditor/plugins/imgur/lang/fr.js
@@ -0,0 +1,6 @@
+CKEDITOR.plugins.setLang('imgur', 'fr', {
+    clientIDMissing: 'Ajoutez config.imgurClientID à votre fichier de configuration /ckeditor/config.js',
+    uploading: ' les images sont en cours de téléchargement ...',
+    failToUpload: 'Echec de téléchargement:',
+    label: 'télécharger les images'
+});
